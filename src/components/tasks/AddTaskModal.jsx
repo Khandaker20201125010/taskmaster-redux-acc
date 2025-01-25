42faffb5ab4bc73851ff307855f04e3b2f247ca6
unknown <prantokih42@gmail.com>
2025-01-25 23:13:56 +0600

    ract hook added

--- src/components/tasks/AddTaskModal.jsx
+++ src/components/tasks/AddTaskModal.jsx
@@ -1,11 +1,19 @@
+import { useForm } from "react-hook-form";
 import Modal from "../ui/Modal";
 
-const AddTaskModal = ({isOpen, setIsOpen}) => {
-    return (
-        <Modal isOpen={isOpen} setIsOpen={setIsOpen} title="Add Task">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis nobis corrupti optio eius modi qui unde ab aspernatur ea ullam voluptatibus doloremque ut exercitationem velit esse, labore quibusdam quis nesciunt.</p>
-      </Modal>
-    );
+const AddTaskModal = ({ isOpen, setIsOpen }) => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen} title="Add Task">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" { ...register("name") }/>
+        <button type="submit">Submit</button>
+      </form>
+    </Modal>
+  );
 };
 
 export default AddTaskModal;
