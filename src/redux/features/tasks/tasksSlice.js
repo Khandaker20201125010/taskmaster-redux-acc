76641c2c99c0d409b088a12d532b1ecdf7d8e2ea
unknown <prantokih42@gmail.com>
2025-01-27 23:28:27 +0600

    ap task

--- src/redux/features/tasks/tasksSlice.js
+++ src/redux/features/tasks/tasksSlice.js
@@ -11,12 +11,15 @@ const tasksSlice = createSlice({
     reducers: {
      addTask (state,{payload}) {
         if(state.tasks.length === 0){
-            state.tasks.push({id: 1,...payload});
+            state.tasks.push({id: 1,status:'pending' ,...payload});
         }
         else{
         const lastElement =  state.tasks.at(-1).id += 1;
         state.tasks.push({id: lastElement.id + 1,...payload});
         }
+     },
+     removeTask:(state,{payload}) => {
+          state.tasks = state.tasks.filter((task) => task.id !== payload);
      }
 
     }
